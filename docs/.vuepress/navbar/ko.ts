ab19321edbd5944f3748c58d705154141a00f691
Great-Stone <hahohh@gmail.com>
2023-09-19 20:30:47 +0900

    add icon

--- docs/.vuepress/navbar/ko.ts
+++ docs/.vuepress/navbar/ko.ts
@@ -5,22 +5,27 @@ export const koNavbar = navbar([
   { text: "How To", icon: "launch", link: "/00-Howto/" },
   {
     text: "Infra",
+    icon: "computer",
     children: [
       { text: "Infrastructure", link: "/01-Infrastructure/" },
       { text: "Private-Platform", link: "/02-PrivatePlatform/" },
       { text: "Public-Cloud", link: "/03-PublicCloud/" },
     ],
   },
-  { text: "Software", link: "/05-Software/" },
-  { text: "HashiCorp", link: "/04-HashiCorp/" },
-  { text: "Etc.", link: "/06-etc/" },
+  { text: "Software", icon: "code", link: "/05-Software/" },
+  {
+    text: "HashiCorp",
+    icon: "workingDirectory",
+    link: "/04-HashiCorp/"
+  },
+  { text: "Etc.", icon: "flex", link: "/06-etc/" },
   {
     text: "MORE",
+    icon: "more",
     children: [
-      { text: "About", link: "/99-about/01-About.html" },
-      { text: "Thank you", link: "/99-about/02-Thanks.html" },
-      { text: "Go Contribute", link: "https://github.com/docmoa/docs" },
+      { text: "About", icon: "info", link: "/99-about/01-About.html" },
+      { text: "Thank you", icon: "like", link: "/99-about/02-Thanks.html" },
     ],
   },
-  { text: "#Tags", link: "/tag" },
+  { text: "Tags", icon: "tag", link: "/tag" },
 ]);
